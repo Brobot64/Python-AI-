19db0af07b3d370ae9410702855ec01bb72c4364
Brobot64 <50984975+Brobot64@users.noreply.github.com>
2020-03-02 11:37:35 +0100

    Update project4.py

--- project4.py
+++ project4.py
@@ -31,7 +31,7 @@ def wishMe():
     speak("I am Brobot Sir. Please tell me how may I help you")       
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
+    #It takes keyboard input from the user and returns voice output
     print("Waiting...")
     r = input(str(""))
     audio = r
